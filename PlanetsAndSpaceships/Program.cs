b861f52dbff6706fb5edfacc3c743db0f782a157
feven90 <f.mulugeta90@gmail.com>
2020-04-06 16:45:01 -0500

    done

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -34,12 +34,39 @@ namespace PlanetsAndSpaceships
 
             var rockyPlanets = planetList.GetRange(0, 4);
 
-            Console.WriteLine(String.Join(", ", rockyPlanets));
+            Console.WriteLine(String.Join(", ", rockyPlanets) + " are the rocky planets");
 
             planetList.Remove("Pluto");
 
             Console.WriteLine(String.Join(", ", planetList));
             Console.WriteLine("Wait, where's Pluto? That's messed up.");
+
+            var spaceships = new Dictionary<string, List<string>>
+            {
+                { "Mariner 10", new List<string> { "Mercury", "Venus", "Earth" } },
+                { "Opportunity", new List<string> { "Mars" } },
+                { "Voyager 1", new List<string> { "Earth", "Jupiter", "Saturn" } },
+                { "Voyager 2", new List<string> { "Earth", "Jupiter", "Saturn", "Uranus", "Neptune" } }
+            };
+
+            var ships = spaceships.Keys;
+            Console.WriteLine(String.Join(", ", ships));
+
+            foreach (var planet in planetList)
+            {
+                var listOfShips = new List<string>();
+
+                foreach (var ship in ships)
+                {
+                    var planetsVisited = spaceships.GetValueOrDefault(ship);
+                    if (planetsVisited.Contains(planet))
+                    {
+                        listOfShips.Add(ship);
+                    }
+                }
+
+                Console.WriteLine($"{planet}: {String.Join(", ", listOfShips)}");
+            }
         }
     }
 }
